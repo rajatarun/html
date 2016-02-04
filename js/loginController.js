c27285d662df0f995a1c82700f1165952bf2dbab
rajatarun <traja1@binghamton.edu>
2016-02-04 14:56:14 -0500

    look

---
+++ js/loginController.js
@@ -0,0 +1,12 @@
+(function() {
+	var app = angular.module("loginController",[]);
+	app.factory("loginService",function(){
+		return {
+			
+		}
+	});
+	app.controller("loginController",function(){
+
+	});
+
+})();
